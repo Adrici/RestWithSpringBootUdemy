d200133f541d0ca5d89406a955286d1c24df810a
unknown <cirellia5@gmail.com>
2020-12-21 17:28:41 -0300

    Hello Spring Boot and Rest Udemy

--- rest-with-springboot-udemy/src/main/java/br/com/erudio/GreetingController.java
+++ rest-with-springboot-udemy/src/main/java/br/com/erudio/GreetingController.java
@@ -10,10 +10,9 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong(); 
 	
-@RequestMapping("/greeting")
+@RequestMapping("/greeting") 
 public Greeting greeting(@RequestParam(value="name", defaultValue="world") String name) {
 	return new Greeting(counter.incrementAndGet(), String.format(template, name));
-		
-		 
+			 
 	}
 }
